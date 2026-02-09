58273f9fb748673cccdfe0527ee59a963574504a
Oleg Boodko <dafsas321@gmail.com>
2026-02-09 14:35:04 +0200

    Refactor entityUser validation has been removed

--- NetShopAPI/Models/User.cs
+++ NetShopAPI/Models/User.cs
@@ -6,33 +6,15 @@ namespace NetShopAPI.Models
     public class User
     {
         public Guid Id { get; set; }
-
-        [MaxLength(100)]
         public string FirstName { get; set; } = null!;
-
-        [MaxLength(100)]
         public string LastName { get; set; } = null!;
-
-        [MaxLength(320)]
         public string Email { get; set; } = null!;
-
-        [Required]
-        [MaxLength(100)]
         public string SurName { get; set; } = null!;
-
-        [Required]
-        [MaxLength(60)]
         public string NickName { get; set; } = null!;
-
-        [Phone]
-        [MaxLength(30)]
-        public string Phone {  get; set; }
-
+        public string? Phone { get; set; }
         public string PasswordHash { get; set; } = null!;
-
-        [MaxLength(30)]
         public string Role {  get; set; } = "User";
-
-        public DateTime CreatedAtUtc { get; set; }
+        public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+        public DateTime? LastAuthorizedtUtc { get; set; }
     }
 }
