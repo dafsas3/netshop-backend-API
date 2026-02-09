be7b55ae6dcf480bed023ac4b62dc0c6169d2fa0
Oleg Boodko <dafsas321@gmail.com>
2026-02-09 14:16:47 +0200

    Refactor CategoriesController

--- NetShopAPI/Controllers/CategoriesController.cs
+++ NetShopAPI/Controllers/CategoriesController.cs
@@ -1,11 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using NetShopAPI.Data;
-using NetShopAPI.DTOs.CatalogDTO.Categoryes.Request;
-using NetShopAPI.DTOs.CatalogDTO.Categoryes.Response;
-using NetShopAPI.Models;
-using NetShopAPI.Services.CatalogServices;
+﻿using Microsoft.AspNetCore.Mvc;
+using NetShopAPI.Common;
+using NetShopAPI.Features.Catalog.Categories.Command;
+using NetShopAPI.Features.Catalog.Categories.Query;
 
 namespace NetShopAPI.Controllers
 {
@@ -14,27 +10,29 @@ namespace NetShopAPI.Controllers
     public class CategoriesController : ControllerBase
     {
 
-        private readonly ICategoryService _categoryService;
+        private readonly CreateCategoryHandler _createHandler;
+        private readonly GetAllCategoriesHandler _getHandler;
 
-        public CategoriesController(ICategoryService service)
+        public CategoriesController(CreateCategoryHandler createHandler, GetAllCategoriesHandler getHandler)
         {
-            _categoryService = service;
+            _createHandler = createHandler;
+            _getHandler = getHandler;
         }
 
 
 
         [HttpPost("create")]
-        public async Task<IActionResult> CreateCategory(CategoryRequest req, CancellationToken ct)
+        public async Task<IActionResult> CreateCategory(CreateCategoryCommand cmd, CancellationToken ct)
         {
-            var result = await _categoryService.CreateCategoryAsync(req, ct);
-            return Ok(result);
+            var result = await _createHandler.Handle(cmd, ct);
+            return this.ToActionResult(result);
         }
 
 
         [HttpGet("get-all")]
         public async Task<IActionResult> GetAllCategories(CancellationToken ct)
         {
-            return Ok(await _categoryService.GetAllAsync(ct));
+            return this.ToActionResult(await _getHandler.Handle(ct));
         }
 
 
